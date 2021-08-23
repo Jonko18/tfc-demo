2d1c6027c0fb0cfef657a9f0233cb1c41eeaa1a6
Jonko18 <Jonko18@users.noreply.github.com>
2021-08-23 14:44:24 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,12 +28,12 @@ resource "aws_instance" "ubuntu" {
   }
 }
 
-//resource "aws_instance" "ubuntu_secondary" {
-//  ami           = data.aws_ami.ubuntu.id
-//  instance_type = var.instance_type
-//
-//  tags = {
-//    Name                 = "${var.instance_name}-secondary"
-//    "Linux Distribution" = "Ubuntu"
-//  }
-//}
+resource "aws_instance" "ubuntu_secondary" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name                 = "${var.instance_name}-secondary"
+    "Linux Distribution" = "Ubuntu"
+  }
+}
